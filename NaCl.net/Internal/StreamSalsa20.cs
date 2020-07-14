faf90bc0a9df9f2f03326bd32bd879aab8ddee79
Jesper Glintborg <jesper.glintborg@siemensgamesa.com>
2020-07-14 21:39:50 +0200

    Improved memory performance.

--- NaCl.net/Internal/StreamSalsa20.cs
+++ NaCl.net/Internal/StreamSalsa20.cs
@@ -11,25 +11,24 @@ namespace NaCl.Internal
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         static public void Transform(Span<byte> c, ReadOnlySpan<byte> n, ReadOnlySpan<byte> k)
         {
-            Span<byte> input = new byte[16];
-            Span<byte> block = new byte[64];
-            Span<byte> kcopy = new byte[32];
-
             if (c.IsEmpty)
                 return;
 
-            for (int i = 0; i < 32; i++)
-                kcopy[i] = k[i];
-            
+            var input = new byte[16];
+            var block = new byte[64];
+            var kcopy = k.ToArray();
+
             for (int i = 0; i < 8; i++)
                 input[i] = n[i];
             
-            for (int i = 8; i < 16; i++)
-                input[i] = 0;
-            
             while (c.Length >= 64)
             {
-                Salsa20.Transform(c, input, kcopy, Span<byte>.Empty);
+                Salsa20.Transform(block, input, kcopy, null);
+
+                for (int i = 0; i < c.Length; i++)
+                    c[i] = block[i];
+
+
                 uint u = 1;
                 for (int i = 8; i < 16; i++)
                 {
@@ -43,13 +42,10 @@ namespace NaCl.Internal
 
             if (c.Length != 0)
             {
-                Salsa20.Transform(block, input, kcopy, Span<byte>.Empty);
+                Salsa20.Transform(block, input, kcopy, null);
                 for (int i = 0; i < c.Length; i++)
                     c[i] = block[i];
             }
-
-            block.Clear();
-            kcopy.Clear();
         }
     }
 }
