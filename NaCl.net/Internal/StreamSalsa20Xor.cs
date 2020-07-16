7b11a80d4c5a98e46f4354a4957a5b0bd0a5b194
Jesper Glintborg <jesper.glintborg@siemensgamesa.com>
2020-07-16 22:27:44 +0200

    Fixed unit test error and clearing private data in memory.

--- NaCl.net/Internal/StreamSalsa20Xor.cs
+++ NaCl.net/Internal/StreamSalsa20Xor.cs
@@ -12,52 +12,59 @@ namespace NaCl.Internal
             if (m.Length == 0)
                 return;
 
-            var input = new  byte[16];
-            var block = new  byte[64];
+            var input = new byte[16];
+            var block = new byte[64];
             var kcopy = k.Slice(0, 32).ToArray();
-            UInt32 u;
-
-            n.Slice(0, 8).CopyTo(input);
-
-            for (int i = 8; i < 16; i++)
+            try
             {
-                input[i] = (byte) (ic & 0xff);
-                ic >>= 8;
-            }
+                UInt32 u;
 
-            int mlen = m.Length;
+                n.Slice(0, 8).CopyTo(input);
 
-            while (mlen >= 64)
-            {
-                Salsa20.Transform(block, input, kcopy, null);
-
-                for (int i = 0; i < 64; i++)
-                {
-                    c[i] = (byte)(m[i] ^ block[i]);
-                }
-
-                u = 1;
                 for (int i = 8; i < 16; i++)
                 {
-                    u += input[i];
-                    input[i] = (byte) u;
-                    u >>= 8;
+                    input[i] = (byte) (ic & 0xff);
+                    ic >>= 8;
                 }
 
-                mlen -= 64;
-                c = c.Slice(64);
-                m = m.Slice(64);
-            }
+                int mlen = m.Length;
 
-            if (mlen != 0)
-            {
-                Salsa20.Transform(block, input, kcopy, null);
-                for (int i = 0; i < mlen; i++)
+                while (mlen >= 64)
                 {
-                    c[i] = (byte) (m[i] ^ block[i]);
+                    Salsa20.Transform(block, input, kcopy, null);
+
+                    for (int i = 0; i < 64; i++)
+                    {
+                        c[i] = (byte)(m[i] ^ block[i]);
+                    }
+
+                    u = 1;
+                    for (int i = 8; i < 16; i++)
+                    {
+                        u += input[i];
+                        input[i] = (byte) u;
+                        u >>= 8;
+                    }
+
+                    mlen -= 64;
+                    c = c.Slice(64);
+                    m = m.Slice(64);
                 }
+
+                if (mlen != 0)
+                {
+                    Salsa20.Transform(block, input, kcopy, null);
+                    for (int i = 0; i < mlen; i++)
+                    {
+                        c[i] = (byte) (m[i] ^ block[i]);
+                    }
+                }
+            }
+            finally
+            {
+                Array.Clear(block, 0, block.Length);
+                Array.Clear(kcopy, 0, kcopy.Length);
             }
         }
-
     }
 }
